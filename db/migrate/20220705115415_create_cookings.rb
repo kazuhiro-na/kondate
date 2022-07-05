4e0c659b77cc65396253cc21b251d9719abc4e8b
kazuhiro-na <narakhope@gmail.com>
2022-07-05 21:02:31 +0900

    cookingsテーブルにカラムを追加

--- db/migrate/20220705115415_create_cookings.rb
+++ db/migrate/20220705115415_create_cookings.rb
@@ -1,7 +1,9 @@
 class CreateCookings < ActiveRecord::Migration[6.0]
   def change
     create_table :cookings do |t|
-
+      t.string    :name, null: false
+      t.integer   :category
+      t.reference :user, null: false, foreign_key: true
       t.timestamps
     end
   end
