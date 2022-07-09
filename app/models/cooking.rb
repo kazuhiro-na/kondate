b0e4b4bbd606279ea3cc889aa97544957be268ca
kazuhiro-na <narakhope@gmail.com>
2022-07-09 18:46:16 +0900

    formオブジェクトにバリデーションを定義

--- app/models/cooking.rb
+++ app/models/cooking.rb
@@ -5,9 +5,4 @@ class Cooking < ApplicationRecord
   has_one :ingredient
   has_one_attached :image
 
-  with_options presence: true do
-    validates :image
-    validates :name
-    validates :category
-  end
 end
--- app/models/cooking_ingredient.rb
+++ app/models/cooking_ingredient.rb
@@ -2,3 +2,9 @@ class CookingIngredient
   include ActiveModel::Model
   attr_accessor :image, :name, :category, :ingredient1, :ingredient2, :ingredient3, :ingredient4, :ingredient5, :ingredient6, :ingredient7, :user_id
 end
+with_options presence: true do
+  validates :image
+  validates :name
+  validates :category
+  validates :user_id
+end
