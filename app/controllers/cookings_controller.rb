df5c2aaeaf3e247448f1ad12c9d17339940ba06a
kazuhiro-na <narakhope@gmail.com>
2022-07-09 19:01:15 +0900

    formオブジェクトのインスタンスを生成するよう記述

--- app/controllers/cookings_controller.rb
+++ app/controllers/cookings_controller.rb
@@ -3,19 +3,23 @@ class CookingsController < ApplicationController
   end
 
   def new
-    @cooking = Cooking.new
+    @cooking_ingredient = CookingIngredient.new
   end
 
   def create
-    @cooking = Cooking.create(cooking_params)
-    Ingredient.create(ingredient_params)
-    redirect_to root_path
+    @cooking_ingredient = CookingIngredient.new(cooking_params)
+    if @cooking_ingredient.valid?
+      @cooking_ingredient.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def cooking_params
-    params.permit(:name, :category, :image).merge(user_id: current_user.id)
+    params.require(:cooking_ingredient).permit(:image, :name, :category, :ingredient1, :ingredient2, :ingredient3, :ingredient4, :ingredient5, :ingredient6, :ingredient7).merge(user_id: current_user.id)
   end
 
   def ingredient_params
