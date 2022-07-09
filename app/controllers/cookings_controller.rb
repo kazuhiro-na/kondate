eaa10e6c70ae685660483d3cd618e30bea440c35
kazuhiro-na <narakhope@gmail.com>
2022-07-09 18:37:40 +0900

    createアクションを定義

--- app/controllers/cookings_controller.rb
+++ app/controllers/cookings_controller.rb
@@ -5,4 +5,20 @@ class CookingsController < ApplicationController
   def new
     @cooking = Cooking.new
   end
+
+  def create
+    @cooking = Cooking.create(cooking_params)
+    Ingredient.create(ingredient_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def cooking_params
+    params.permit(:name, :category, :image).merge(user_id: current_user.id)
+  end
+
+  def ingredient_params
+    params.permit(:ingredient1, :ingredient2, :ingredient3, :ingredient4, :ingredient5, :ingredient6, :ingredient7).merge(cooking_id: @cooking.id)
+  end
 end
