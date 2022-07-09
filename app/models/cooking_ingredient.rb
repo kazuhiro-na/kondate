a3cb62227ff02a10553e22f8e19d393e14f1d0ac
kazuhiro-na <narakhope@gmail.com>
2022-07-09 18:43:34 +0900

    formオブジェクトのクラスを作成

---
+++ app/models/cooking_ingredient.rb
@@ -0,0 +1,4 @@
+class CookingIngredient
+  include ActiveModel::Model
+  attr_accessor :image, :name, :category, :ingredient1, :ingredient2, :ingredient3, :ingredient4, :ingredient5, :ingredient6, :ingredient7, :user_id
+end
