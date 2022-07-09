bebd67af379fedfd1068d697f2354d2c317ddba0
kazuhiro-na <narakhope@gmail.com>
2022-07-09 18:51:33 +0900

    データをテーブルに保存する処理を記述

--- app/models/cooking_ingredient.rb
+++ app/models/cooking_ingredient.rb
@@ -1,10 +1,15 @@
 class CookingIngredient
   include ActiveModel::Model
   attr_accessor :image, :name, :category, :ingredient1, :ingredient2, :ingredient3, :ingredient4, :ingredient5, :ingredient6, :ingredient7, :user_id
-end
-with_options presence: true do
-  validates :image
-  validates :name
-  validates :category
-  validates :user_id
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :category
+    validates :user_id
+  end
+
+  def save
+    cooking = Cooking.create(image: image, name: name, category: category, user_id: user_id)
+    Ingredient.create(ingredient1: ingredient1, ingredient2: ingredient2, ingredient3: ingredient3, ingredient4: ingredient4, ingredient5: ingredient5, ingredient6: ingredient6, ingredient7: ingredient7, cooking_id: cooking.id)
 end
