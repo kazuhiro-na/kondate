cef1f71822fd764defaff179fe09060482860d00
kazuhiro-na <narakhope@gmail.com>
2022-07-20 14:05:53 +0900

    未ログインのユーザーが詳細ページへアクセスできないよう設定

--- app/controllers/cookings_controller.rb
+++ app/controllers/cookings_controller.rb
@@ -1,5 +1,5 @@
 class CookingsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :show]
   def index
     if user_signed_in?
       cookings = Cooking.where(user_id: current_user.id)
