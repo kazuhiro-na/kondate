19aec1098df91a77df32552ead5f4e4edda6e46f
kazuhiro-na <narakhope@gmail.com>
2022-08-02 22:20:14 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "cookings#index"
-  resources :cookings, only: [:index, :new, :create, :show, :destroy]
+  resources :cookings, only: [:index, :new, :create, :show, :destroy, :edit]
   resources :users, only: :show
 end
