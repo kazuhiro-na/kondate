d2e13da977ae1d5274c6bac50b45a0f8269b2a92
kazuhiro-na <narakhope@gmail.com>
2022-08-10 14:27:34 +0900

    updateアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "cookings#index"
-  resources :cookings, only: [:index, :new, :create, :show, :destroy, :edit]
+  resources :cookings, only: [:index, :new, :create, :show, :destroy, :edit, :update]
   resources :users, only: :show
 end
