d67b9773f0caff02a7565598c544ca8cb715f564
kazuhiro-na <narakhope@gmail.com>
2022-08-21 23:03:20 +0900

    attr_accessorにcooking_idを追加

--- app/models/cooking_ingredient.rb
+++ app/models/cooking_ingredient.rb
@@ -2,7 +2,7 @@ class CookingIngredient
   include ActiveModel::Model
   attr_accessor :image, :name, :category_id, :ingredient1, :ingredient2, :ingredient3, :ingredient4, :ingredient5, :ingredient6,
                 :ingredient7, :user_id,
-                :id, :created_at, :datetime, :updated_at, :datetime
+                :id, :cooking_id, :created_at, :datetime, :updated_at, :datetime
 
   with_options presence: true do
     validates :image
